33fb4d22c243d8607761fae186bb9c7719a3c255
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-22 12:13:50 +0100

    create contract class

--- src/main/java/com/app/models/EquipmentDemande.java
+++ src/main/java/com/app/models/EquipmentDemande.java
@@ -1,6 +1,5 @@
 package com.app.models;
 
-import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
