59d01fa27434d1f66f71364eede9ab4e7098bdce
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-22 15:12:24 +0100

    add class document and add picture to equipment class

--- src/main/java/com/app/models/User.java
+++ src/main/java/com/app/models/User.java
@@ -1,14 +1,19 @@
 package com.app.models;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.context.support.BeanDefinitionDsl.Role;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,17 +27,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
 
-	@Id // pour spécifier la clé primaire
-	// pour spécifier la maniéere de géneartion de la clé primaire
+	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@NotBlank(message="Name is required")
 	@Size(max=100,message="Name must be at most 100 characters")
 	private String name;
+	
 	@NotBlank(message="Email is required")
 	@Email(message="Invalid email format")
 	@Size(max=255,message="email must be at most 255 characters")
 	@Column(unique = true)
 	private String email;
+	@NotNull(message="role is required")
+	
+	@NotNull(message="role is required")
+	private Role role;
 }
