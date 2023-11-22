60c41458122368fe655c38cc3c328bc134b96fbc
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-22 11:06:18 +0100

    add user feild with validation for demande class

--- src/main/java/com/app/models/Demande.java
+++ src/main/java/com/app/models/Demande.java
@@ -1,7 +1,14 @@
 package com.app.models;
 
+
 import com.app.models.enums.DemandeStatus;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +16,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="demandes")
 public class Demande {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private DemandeStatus demandeStatus;
 	
+    @NotNull(message ="Status is required")
+	private DemandeStatus demandeStatus;
+    
+    @NotNull(message ="user is required")
+	private User user;
 }
